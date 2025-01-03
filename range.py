496f0985ec3a24a4e30bbddf6835779389a0fedb
Gopaldr <gopalreddydesireddy2004@gmail.com>
2025-01-03 15:53:56 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,12 @@
+range1=range(40+1)
+for i in range1:
+    print(i)
+print("------------------------------------")
+range2=range(40+1,80+1)
+for i in range2:
+    print(i)
+print("------------------------------------")
+range3=range(40+1,80+1,6)
+for i in range3:
+    print(i)
+print("------------------------------------")
